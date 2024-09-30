902c0498d9fe57da3338116eafeb0ceeef2c0e3c
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-09-30 14:30:44 -0300

    chore: ⚙️ package and lint settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert'],
+    ],
     'header-max-length': [1, 'always', 120],
     'body-max-length': [0, 'always', 120],
     'body-max-line-length': [0, 'always', 120],
   },
-};
+}
